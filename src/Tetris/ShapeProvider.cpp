49d78783ccdf9aaeb6ef8734ca089673db9be92c
Martin <martinmalar@gmail.com>
2017-07-06 00:10:31 +0200

    empty blocks need -1 in static shape data and other fixes

--- src/Tetris/ShapeProvider.cpp
+++ src/Tetris/ShapeProvider.cpp
@@ -6,7 +6,7 @@ Shape& ShapeProvider::getRandom() {
     const unsigned shapesCount = 5;
     auto r = rnd.get(1, shapesCount);
 
-    switch(r) {
+    switch (r) {
         case 1: return shapeI;
         case 2: return rnd.get(0, 1) ? shapeJ : shapeL;
         case 3: return shapeT;
@@ -20,95 +20,95 @@ RandomProvider ShapeProvider::rnd;
 
 Shape ShapeProvider::shapeI {
     Array2D<blockId>{{
-            {1, 1, 1, 1}
+            {6, 6, 6, 6}
         }},
     Array2D<blockId>{{
-            {1}, {1}, {1}, {1}
+            {6}, {6}, {6}, {6}
         }}
 };
 
 Shape ShapeProvider::shapeJ {
     Array2D<blockId>{{
-            {0, 2},
-            {0, 2},
-            {2, 2}
+            {-1,  2},
+            {-1,  2},
+            { 2,  2}
         }},
     Array2D<blockId>{{
-            {2, 0, 0},
-            {2, 2, 2}
+            {2, -1, -1},
+            {2,  2,  2}
         }},
     Array2D<blockId>{{
-            {2, 2},
-            {2, 0},
-            {2, 0}
+            {2,  2},
+            {2, -1},
+            {2, -1}
         }},
     Array2D<blockId>{{
-            {2, 2, 2},
-            {0, 0, 2}
+            { 2,  2, 2},
+            {-1, -1, 2}
         }}
 };
 
 Shape ShapeProvider::shapeL {
     Array2D<blockId>{{
-            {2, 0},
-            {2, 0},
-            {2, 2}
+            {2, -1},
+            {2, -1},
+            {2,  2}
         }},
     Array2D<blockId>{{
-            {2, 2, 2},
-            {2, 0, 0}
+            {2,  2,  2},
+            {2, -1, -1}
         }},
     Array2D<blockId>{{
-            {2, 2},
-            {0, 2},
-            {0, 2}
+            {2,  2},
+            {-1, 2},
+            {-1, 2}
         }},
     Array2D<blockId>{{
-            {0, 0, 2},
-            {2, 2, 2}
+            {-1, -1, 2},
+            { 2,  2, 2}
         }}
 };
 
 Shape ShapeProvider::shapeT {
     Array2D<blockId>{{
-            {0, 3, 0},
-            {3, 3, 3}
+            {-1, 3, -1},
+            { 3, 3,  3}
         }},
     Array2D<blockId>{{
-            {3, 0},
-            {3, 3},
-            {3, 0}
+            {3, -1},
+            {3,  3},
+            {3, -1}
         }},
     Array2D<blockId>{{
-            {3, 3, 3},
-            {0, 3, 0}
+            { 3, 3,  3},
+            {-1, 3, -1}
         }},
     Array2D<blockId>{{
-            {0, 3},
-            {3, 3},
-            {0, 3}
+            {-1, 3},
+            { 3, 3},
+            {-1, 3}
         }}
 };
 Shape ShapeProvider::shapeZ {
     Array2D<blockId>{{
-            {4, 4, 0},
-            {0, 4, 4}
+            { 4,  4, -1},
+            {-1,  4,  4}
         }},
     Array2D<blockId>{{
-            {0, 4},
-            {4, 4},
-            {4, 0}
+            {-1,  4},
+            { 4,  4},
+            { 4, -1}
         }}
 };
 Shape ShapeProvider::shapeS {
     Array2D<blockId>{{
-            {0, 4, 4},
-            {4, 4, 0}
+            {-1, 4,  4},
+            { 4, 4, -1}
         }},
     Array2D<blockId>{{
-            {4, 0},
-            {4, 4},
-            {0, 4}
+            { 4, -1},
+            { 4,  4},
+            {-1,  4}
         }}
 };
 Shape ShapeProvider::shapeO {
